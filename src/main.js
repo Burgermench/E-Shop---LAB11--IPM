0b2a6a5cbc21f814927cf4b96425fec4c049142a
Burgermench <66210216+Burgermench@users.noreply.github.com>
2022-12-05 12:04:21 +0000

    Holy mother of fixed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // Import code from other modules
 import Vue from 'vue'              // Import vue class      (to use vue object)
 import App from './App.vue'        // Import App component  (centralizes all components)         [used by vue object]
-import store from './store'        // Import router         (links relative paths to components) [used by App component]
 import router from './router'      // Import store          (container for application state)    [used by vue object]
+import store from './store'        // Import router         (links relative paths to components) [used by App component]
 
 // Prevents the production tip on Vue startup
 Vue.config.productionTip = false
