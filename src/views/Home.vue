ca9e8692b35c862ccd06f0a3152609c64dafdaa2
Burgermench <66210216+Burgermench@users.noreply.github.com>
2022-12-07 13:40:17 +0000

    Building home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,28 @@
 <template>
   <div id="app">
-	<div :style="image"></div>
+	<v-img
+		contain
+		lazy-src="@/assets/images/home_background.jpg" 
+		max-height="280" 
+		max-width="482" 
+		src="@/assets/images/home_background.jpg"
+		id="background_image"
+	>
+		<div>
+			<h1 class="title is-1">
+				Your Window
+			</h1>
+			<h3 class="title is-3">
+				Width: {{ window.width }} px<br/>
+				Height: {{ window.height }} px
+			</h3>
+			<p class="has-text-white">
+				&uarr;<br/>
+				&larr; resize window &rarr;<br/>
+				&darr;
+			</p>
+		</div>
+	</v-img>
     <h1>{{ msg }}</h1>
   </div>
 </template>
@@ -23,10 +45,20 @@ export default {
 		}
 	},
 	mounted() {
-
+		
+	},
+	created() {
+		window.addEventListener('resize', this.handleResize);
+		this.handleResize();
+	},
+	destroyed() {
+		window.removeEventListener('resize', this.handleResize);
 	},
 	methods: {
-
+		handleResize() {
+			this.window.width = window.innerWidth;
+			this.window.height = window.innerHeight;
+		}
 	},
 	computed: {
 
@@ -37,19 +69,26 @@ export default {
 
 
 
-<style scoped>
+<style lang="scss" scoped>
+$top-color: yellow;
+$bottom-color: tomato;
+
+html,
+#app,
+
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	background-image: url('../assets/images/home_background.jpg');
-	background-repeat: no-repeat;
-	background-size: contain;
 }
 
-@media screen and (max-width: 600px) {
-	#app {
-		margin-top: 0;
-	}	
+#showoff {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+	color: black;	
+    background: linear-gradient($top-color, $bottom-color);
 }
+
 </style>
