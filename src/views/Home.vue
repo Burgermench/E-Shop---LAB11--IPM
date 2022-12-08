60512ddec268d452d036462d29650a01fae825bb
Burgermench <66210216+Burgermench@users.noreply.github.com>
2022-12-08 14:01:28 +0000

    Fixing Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,21 @@
 <template>
-  <div id="app">
+	<div id="app">
 		<div id="content-wrapper">
-			<h1>
-				{{ background.image }}
-			</h1>
-			<h3 class="title is-3">
+			
+			<img id="bckg_img">
+
+			<div id="r1c1">
+				&rarr; <button id="cpu_btn"><img id="cpu_img"></button> &larr;
+			</div>
+			
+			<div id="debug">
+				{{ msg }}<br>
 				Width: {{ window.width }} px<br/>
 				Height: {{ window.height }} px
-			</h3>
-			<p class="has-text-white">
-				&uarr;<br/>
-				&larr; resize window &rarr;<br/>
-				&darr;
-			</p>
+			</div>
+		
 		</div>
-    <h1>{{ msg }}</h1>
-  </div>
+	</div>
 </template>
 
 
@@ -24,40 +24,42 @@
 
 
 <script>
+
 export default {
 	name: "Home",
+
 	data() {
 		return {
 			window: {
-				width: 0,
-				height: 0
+				width: 50,
+				height: 50,
 			},
-			background: {
-				image: Image,
-				width: window.width,
-				height: window.height
-			},
-			msg: 'Welcome to Your LAB11 Vue App',
+			msg: 'Debug msg',
 		}
 	},
-	mounted() {
-		console.clear();
-		this.background.image = new Image(this.background.width, this.background.height);
-		this.background.image.src = '@/assets/images/home_background.jpg';
-	},
+
 	created() {
+		console.clear();
 		window.addEventListener('resize', this.handleResize);
-		this.handleResize();
 	},
+
+	mounted() {
+		this.resize();
+		document.getElementById('bckg_img').src = require('@/assets/images/home_background.jpg');
+		document.getElementById('cpu_img').src = require('@/assets/images/cpu.png');
+	},
+
 	destroyed() {
 		window.removeEventListener('resize', this.handleResize);
 	},
+
 	methods: {
-		handleResize() {
+		resize() {
 			this.window.width = window.innerWidth;
 			this.window.height = window.innerHeight;
 		}
 	},
+
 	computed: {
 
 	},
@@ -71,24 +73,79 @@ export default {
 
 
 <style lang="scss" scoped>
-$top-color: yellow;
-$bottom-color: tomato;
+// DEBUG
+$debug: true;
 
-html,
-#app,
+@mixin debug(){
+	border-style: dashed;
+	border-color: red;
+	border-width: 1px;
+}
 
+div {
+	@if $debug {
+		@include debug();
+	}
+}
+
+// APP
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 }
 
+// GRIDS
 #content-wrapper {
-    display: flex;
-    flex-flow: column;
-    justify-content: center;
-    align-items: center;
-	color: black;	
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: 1fr;
+	grid-template-areas: "bckg_img";
 }
 
+#bckg_img {
+	grid-area: bckg_img;
+	position: fixed;
+	width: 100vw;
+	height: 100vh;
+	z-index: -1;
+}
+
+#c1 {
+	position: fixed;
+	width: 50%;
+	height: 100%;
+	margin-left: 25%;
+	margin-right: 25%;
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: 20%, 80%;
+	grid-template-areas: "c1r1" "c1r2";
+}
+
+#c1r1 {
+	grid-area: c1r1;
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	
+}
+
+#c1r2 {
+	grid-area : c1r2;
+	position: fixed;
+	width: 100%;
+	height: 100%;
+}
+
+#cpu_btn {
+	grid-area: cpu_btn;
+	position: fixed;
+	width: 100px;
+	height: 100px;
+	z-index: 1;
+}
 </style>
